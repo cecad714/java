982c98ba1787e0659ee91e78c4023bfc961f81e2
Arun B M <146818469+cecad714@users.noreply.github.com>
2024-03-11 10:35:04 +0530

    Updated col in j looo

--- matrixadd.java
+++ matrixadd.java
@@ -12,26 +12,26 @@ class matrixadd{
 		
 		System.out.println("Enter the elements of the first matrix: ");
 		for(int i = 0; i<row; i++){
-			for(int j = 0; j<row; j++){
+			for(int j = 0; j<col; j++){
 				matrix1[i][j] = scanner.nextInt();
 				}		
 		}		
 		System.out.println("Enter the elements of the second matrix: ");
 		for(int i = 0; i<row; i++){
-			for(int j = 0; j<row; j++){
+			for(int j = 0; j<col; j++){
 				matrix2[i][j] = scanner.nextInt();
 				}		
 		}	
 	
 		int[][] summatrix = new int[row][col];
 		for(int i = 0; i<row; i++){
-			for(int j = 0; j<row; j++){
+			for(int j = 0; j<col; j++){
 				summatrix[i][j] = matrix1[i][j] + matrix2[i][j];
 				}		
 		}
 		System.out.println("Sum of Matrices: ");
 		for(int i = 0; i<row; i++){
-			for(int j = 0; j<row; j++){
+			for(int j = 0; j<col; j++){
 				System.out.print(summatrix[i][j]+" ");
 				}
 				System.out.print("\n");		
