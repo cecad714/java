b46914471341314832d093abae1daf840c58b5cf
Arun B M <146818469+cecad714@users.noreply.github.com>
2024-04-20 16:20:06 +0530

    Update productprice.java
    added a specific function for displaying elements so that it will be easier

--- productprice.java
+++ productprice.java
@@ -1,33 +1,40 @@
+class Product {
+    String pcode, pname;
+    float price;
 
-class product{
-	String pcode,pname;
-	float price;
-	
-	public product(String pcode,String pname,float price){
-		this.pcode=pcode;
-		this.pname=pname;
-		this.price=price;	
-	}
+    public Product(String pcode, String pname, float price) {
+        this.pcode = pcode;
+        this.pname = pname;
+        this.price = price;
+    }
 }
 
-public class productprice{
-	public static void main(String[] args){
-		product p1 = new product("01","IceCream",100);
-		product p2 = new product("02","Biscuit",150);
-		product p3 = new product("03","Cake",200);
-		
-		product lowp = p1;
-				
-		if(p2.price < lowp.price){
-			lowp = p2;	
-		}
+public class ProductPrice {
+    public static void main(String[] args) {
+        Product p1 = new Product("01", "IceCream", 100);
+        Product p2 = new Product("02", "Biscuit", 150);
+        Product p3 = new Product("03", "Cake", 200);
 
-		if(p3.price < lowp.price){
-			lowp = p3;	
-		}
-		System.out.println("The Product with the lowest price:");
-		System.out.println("Product Code: "+lowp.pcode);
-		System.out.println("Product Name: "+lowp.pname);		
-		System.out.println("Product Price: "+lowp.price);
-}
+        System.out.println("All Products:");
+        printProductDetails(p1);
+        printProductDetails(p2);
+        printProductDetails(p3);
+
+        Product lowp = p1;
+        if (p2.price < lowp.price) {
+            lowp = p2;
+        }
+        if (p3.price < lowp.price) {
+            lowp = p3;
+        }
+
+        System.out.println("The Product with the lowest price:");
+        printProductDetails(lowp);
+    }
+
+    public static void printProductDetails(Product product) {
+        System.out.println("Product Code: " + product.pcode);
+        System.out.println("Product Name: " + product.pname);
+        System.out.println("Product Price: " + product.price + "\n");
+    }
 }
